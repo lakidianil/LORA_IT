bf6cd724e442b439523ba575a9ef207f79ccb1c9
lakidianil <144909023+lakidianil@users.noreply.github.com>
2025-06-15 01:32:16 +0530

    Add files via upload

--- App.test.js
+++ App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import Home_Page from './Home_Page';
 
 test('renders learn react link', () => {
-  render(<App />);
+  render(<Home_Page />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
